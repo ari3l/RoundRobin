5880e0cc866a13692ee5855ed5bc0e7ba555d671
Ariel <arielsteinlauf@gmail.com>
2017-12-01 11:21:18 -0500

    Don’t allow a quantum of 0 or less

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,17 @@ public class Main {
     public static CPUDataSource dataSource;
 
     public static void main(String[] args) {
-
         String csvFile = args[0];
+        int quantum = Integer.parseInt(args[1]);
+
+        if (quantum <= 0) {
+            System.out.println("Please try again with a quantum greater than 0");
+            return;
+        }
+
         CSVLoader csvLoader = new CSVLoader(csvFile);
 
         dataSource = new CPUDataSource(csvLoader.read());
-        int quantum = Integer.parseInt(args[1]);
 
         CPU cpu = new CPU(dataSource, quantum);
         cpu.start();
